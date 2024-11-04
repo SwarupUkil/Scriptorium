c44d95848cee0cfffbe0eaea67f33aa36e523f41
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2024-11-04 18:43:02 -0500

    Implement request to update a template

--- scriptorium/src/pages/api/templates/users/edit.js
+++ scriptorium/src/pages/api/templates/users/edit.js
@@ -1,9 +1,103 @@
 import {prisma} from "../../../../utils/db";
 import { verifyTokenMiddleware } from "../../../../utils/auth";
+import {MAX_CODE, MAX_EXPLANATION, MAX_TAGS, MAX_TITLE, PRIVACY} from "../../../../utils/validationConstants";
+import validateTags from "../../../../utils/validateTags";
+import {parseLanguage} from "../../../../utils/validateLanguage";
 
 // Handler will update the clients templates with specified values.
 async function handler(req, res) {
 
+    if (req.method !== "PUT" && req.method !== "DELETE") {
+        return res.status(405).json({message: "Method not allowed"});
+    }
+
+    const { id } = req.body;
+    const templateId = Number(id);
+
+    if (!id) {
+        return res.status(404).json({ error: "Invalid ID: missing blog ID" });
+    }
+
+    if (isNaN(templateId)) {
+        return res.status(400).json({ error: "Invalid ID: not a number" });
+    }
+
+    const user = req.user;
+    const userId = user.id;
+    const { title, explanation, tags, code, language, privacy } = req.body;
+
+    if (title && title.length > MAX_TITLE) {
+        return res.status(400).json({message: "Title is too large"});
+    }
+
+    if (explanation && explanation.length > MAX_EXPLANATION) {
+        return res.status(400).json({message: "Description is too large"});
+    }
+
+    if (code && code.length > MAX_CODE) {
+        return res.status(400).json({message: "Code is too large"});
+    }
+
+    if (typeof tags !== "undefined" && typeof tags !== "string") {
+        return res.status(400).json({message: "Tags must be given as one long CSV styled string"});
+    }
+
+    if (tags && tags.length > MAX_TAGS) {
+        return res.status(400).json({message: `Too many tags, shorten to less then ${MAX_TAGS + 1} characters in CSV form`});
+    }
+
+    if (tags && !validateTags(tags)) {
+        return res.status(400).json({message: "Tags must be given following CSV notation (no spaces)"});
+    }
+
+    const codeLanguage = parseLanguage(language);
+    if (!codeLanguage) {
+        return res.status(400).json({message: "Code language is invalid"});
+    }
+
+    if (privacy && !PRIVACY.includes(privacy)) {
+        return res.status(400).json({message: `Privacy is invalid. Must be from ${PRIVACY}`});
+    }
+
+    // Check if the blog exists and if the user ID matches
+    try {
+        const template = await prisma.template.findUnique({
+            where: {
+                id: templateId,
+                uid: userId,
+            },
+        });
+
+        if (!template || Number(template.uid) !== userId) {
+            return res.status(401).json({ message: "Unauthorized or Blog not found." });
+        }
+    } catch (error) {
+        return res.status(400).json({ message: "An error occurred while authorizing the update template query" });
+    }
+
+    if (req.method === "PUT") {
+        try {
+            await prisma.template.update({
+                where: {
+                    id: templateId,
+                },
+                data: {
+                    code: code,
+                    language: codeLanguage,
+                    title: title,
+                    explanation: explanation,
+                    tags: tags ? tags : undefined,
+                    privacy: privacy ? privacy : PRIVACY[1], // default to private
+                },
+            });
+
+            return res.status(200).json({message: "Successfully updated template"});
+        } catch (error) {
+            return res.status(400).json({ message: "An error occurred while updating the template" });
+        }
+    } else {
+
+    }
 }
 
 export default verifyTokenMiddleware(handler, "USER");
--- scriptorium/src/utils/validationConstants.js
+++ scriptorium/src/utils/validationConstants.js
@@ -4,6 +4,7 @@ export const MAX_TAGS = 100;
 export const MAX_EXPLANATION = 3000;
 export const MAX_BLOG_DESCRIPTION = 3000;
 export const MAX_COMMENT_DESCRIPTION = 3000;
+export const PRIVACY = ["PUBLIC", "PRIVATE"];
 
 export const SUPPORTED_LANGUAGES = {
     JAVASCRIPT: ["javascript", "js", "java script"],
